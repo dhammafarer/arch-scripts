ad597d9a7350f1213c64a864c5591dcce854a0a9
Pawel Lisewski <pawel.lisewski@gmail.com>
2021-04-15 15:39:29 +0800

    update modules/arbites

--- post-install/modules/arbites.sh
+++ post-install/modules/arbites.sh
@@ -7,6 +7,7 @@ echo
 # ------------------------------------------------------------------------
 
 PKGS=(
+    'gtk-sharp-2'
     'java-rxtx'
     'mono'
 )
@@ -23,6 +24,14 @@ echo "ADDING USER TO GROUPS"
 
 sudo usermod -a -G uucp pawel
 sudo usermod -a -G lock pawel
+
+# ------------------------------------------------------------------------
+echo "DOWNLOADING ARBITES"
+
+wget "https://github.com/blahlicus/arbites-family/releases/download/2-3-5/Arbites.Innova.2-3-5.Gtk2.zip" -O temp.zip
+unzip temp.zip -d ~/Downloads
+rm temp.zip
+ln -fs ~/Nextcloud/AppData/arbites/pawel-DTM2-swapped-tab.arb2dtm ~/Downloads/'Arbites Innova 2-3-5 Gtk2'/
 
 # ------------------------------------------------------------------------
 
